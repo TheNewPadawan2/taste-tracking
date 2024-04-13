b8d42dc22587c0b6213baaefbb85e09735c973af
TheNewPadawan2 <thenewpadawangithub@gmail.com>
2024-04-13 10:04:45 +0200

    Toolbar

--- tt-fe/src/app/app.module.ts
+++ tt-fe/src/app/app.module.ts
@@ -5,13 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [
     provideClientHydration(),
