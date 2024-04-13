6d9350fe7f8e0830acdfc85d67f81dd96b391d5b
TheNewPadawan2 <thenewpadawangithub@gmail.com>
2024-04-13 10:35:16 +0200

    Sidenav

--- tt-fe/src/app/app.module.ts
+++ tt-fe/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
@@ -18,6 +20,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     AppRoutingModule,
     MatButtonModule,
     MatIconModule,
+    MatListModule,
+    MatSidenavModule,
     MatToolbarModule
   ],
   providers: [
