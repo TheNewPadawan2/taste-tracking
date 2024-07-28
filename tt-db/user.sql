f5e61f28dcb71be20f86b32435cca5c1a8680fd2
TheNewPadawan2 <thenewpadawangithub@gmail.com>
2024-07-28 16:23:40 +0200

    DB user table

---
+++ tt-db/user.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `taste_tracking_db`.`user` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `firstname` VARCHAR(64) NOT NULL,
+    `lastname` VARCHAR(64) NOT NULL,
+    `birthday` DATE NOT NULL,
+    `tax` VARCHAR(16) NOT NULL,
+    `email` VARCHAR(320) NOT NULL,
+    `password` VARCHAR(60) NOT NULL,
+    `phone_number` VARCHAR(25) NOT NULL,
+    `verfied` BOOLEAN NULL DEFAULT FALSE,
+    `creation_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE `UNQ_EMAIL` (`email`));
