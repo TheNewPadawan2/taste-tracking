b0bed99b8e31637f638c18ddd7468aa9affc0c6c
TheNewPadawan2 <thenewpadawangithub@gmail.com>
2024-07-28 16:44:25 +0200

    BE user mapper

---
+++ tt-be/login/src/main/java/it/uniecampus/lfulgione/tastetracking/ttbe/login/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package it.uniecampus.lfulgione.tastetracking.ttbe.login.mapper;
+
+import it.uniecampus.lfulgione.tastetracking.ttbe.login.dto.SignupRequestDTO;
+import it.uniecampus.lfulgione.tastetracking.ttbe.login.entity.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+    @Mapping(source = "taxId", target = "tax")
+    UserEntity entity(SignupRequestDTO dto);
+
+    @Mapping(source = "tax", target = "taxId")
+    SignupRequestDTO dto(UserEntity entity);
+}
