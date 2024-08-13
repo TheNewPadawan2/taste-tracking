02b94cfc95cfe220f099513d124b55d5174ba3a1
TheNewPadawan2 <thenewpadawangithub@gmail.com>
2024-08-13 21:56:15 +0200

    FE improved module

--- tt-fe/src/app/app.module.ts
+++ tt-fe/src/app/app.module.ts
@@ -1,23 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatNativeDateModule} from '@angular/material/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {MatCardModule} from '@angular/material/card'
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatCardModule } from '@angular/material/card'
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+//import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
-import {HttpClientModule} from '@angular/common/http';
 import { SidenavContentComponent } from './sidenav-content/sidenav-content.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -29,24 +30,26 @@ import { SignupComponent } from './signup/signup.component';
     SignupComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
     HttpClientModule,
     MatButtonModule,
     MatCardModule,
     MatDatepickerModule,
+    //MatExpansionModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
     MatNativeDateModule,
     MatSidenavModule,
+    MatTableModule,
     MatToolbarModule
   ],
   providers: [
-    provideClientHydration(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
