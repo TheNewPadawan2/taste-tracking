f570426fdf919ce42a5689ad3a2ce4d12038ebea
TheNewPadawan2 <thenewpadawangithub@gmail.com>
2024-09-07 18:38:26 +0200

    FE: Pair sequence

--- tt-fe/src/app/_pipe/pair-sequence-match.pipe.ts
+++ tt-fe/src/app/_pipe/pair-sequence-match.pipe.ts
@@ -17,7 +17,9 @@ export class PairSequenceMatchPipe implements PipeTransform {
       }
     }
     for (let i = 0; i < array1.length; i++) {
-      if (array0[i] !== array1[i]) return false;
+      if (typeof array0[i] === 'string' && typeof array1[i] === 'string') {
+        if ((array0[i] as string).toLowerCase().trim() !== (array1[i] as string).toLowerCase().trim()) return false;
+      } else if (array0[i] !== array1[i]) return false;
     }
     return true;
   }
