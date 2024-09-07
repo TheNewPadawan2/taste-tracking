01155084d868e5dcddffcf85f1c43b9e5004c933
TheNewPadawan2 <thenewpadawangithub@gmail.com>
2024-09-07 17:32:51 +0200

    FE: Product Update endpoint call

--- tt-fe/src/app/product-dialog/product-dialog.component.ts
+++ tt-fe/src/app/product-dialog/product-dialog.component.ts
@@ -1,8 +1,10 @@
 import { Component, Inject } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Product } from '../_dto/product';
 import { PRODUCT_TYPES } from '../_mock/mock-product-type';
 import { KeyValue } from '../_dto/key-value';
+import { ProductService } from '../_service/product.service';
 
 @Component({
   selector: 'app-product-dialog',
@@ -14,9 +16,26 @@ export class ProductDialogComponent {
   product: Product;
   productUpdated: Product;
 
-  constructor(@Inject(MAT_DIALOG_DATA) private data: Product) {
+  constructor(
+          private productService: ProductService,
+          @Inject(MAT_DIALOG_DATA) private data: Product) {
     this.product = data;
     this.productUpdated = { name: this.product.name, type: this.product.type };
+  }
+
+  update(): void {
+    this.productService.update(this.product, this.productUpdated).subscribe({
+      next: (): void => {
+        console.log("success");
+      },
+      error: (res: HttpErrorResponse): void => {
+        console.error(res.status);
+        console.error(res.error);
+      },
+      complete: (): void => {
+        console.log("complete");
+      }
+    });
   }
 
 }
